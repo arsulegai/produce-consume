9eaf2a8979685c1e0d208fe998bb1769f28d3893
manju956 <manjunath.a.c@intel.com>
2020-02-29 13:28:14 -0800

    [Temporary] Remove Sabre and keep Sawtooth
    Signed-off-by: manju956 <manjunath.a.c@intel.com>

--- processor/src/produce_consume/mod.rs
+++ processor/src/produce_consume/mod.rs
@@ -15,6 +15,7 @@
 pub mod handler;
 pub(crate) mod payload;
 
+/*
 cfg_if! {
     if #[cfg(target_arch = "wasm32")] {
         use sabre_sdk::{WasmPtr, execute_entrypoint};
@@ -59,3 +60,4 @@ pub unsafe fn entrypoint(payload: WasmPtr, signer: WasmPtr, signature: WasmPtr) 
     // Implement the apply method, this is the core business logic part of the Sabre
     execute_entrypoint(payload, signer, signature, apply)
 }
+*/
